c45f1aa99f0a5df5c12e3ccf5ce2959e6a8140cf
Abhishekr2122 <abhishekrshinde21@gmail.com>
2023-12-14 16:44:38 +0530

    Displaying final country list

--- src/ui/Modal.js
+++ src/ui/Modal.js
@@ -14,228 +14,39 @@ export default function Modal({ children }) {
   const [value, setValue] = useState("");
   console.log(value.length);
 
-  const data = [
-    "Portugal",
-    "Palau",
-    "Paraguay",
-    "Qatar",
-    "Andorra",
-    "United Arab Emirates",
-    "Afghanistan",
-    "Antigua And Barbuda",
-    "Albania",
-    "Armenia",
-    "Angola",
-    "Argentina",
-    "Austria",
-    "Australia",
-    "Aruba",
-    "Åland Islands",
-    "Azerbaijan",
-    "Romania",
-    "Bosnia And Herzegovina",
-    "Barbados",
-    "Serbia",
-    "Bangladesh",
-    "Russia",
-    "Belgium",
-    "Burkina Faso",
-    "Rwanda",
-    "Bulgaria",
-    "Bahrain",
-    "Burundi",
-    "Benin",
-    "Brunei",
-    "Bolivia",
-    "Saudi Arabia",
-    "Solomon Islands",
-    "Seychelles",
-    "Brazil",
-    "Sudan",
-    "The Bahamas",
-    "Bhutan",
-    "Sweden",
-    "Singapore",
-    "Botswana",
-    "Slovenia",
-    "Belarus",
-    "Slovakia",
-    "Belize",
-    "Sierra Leone",
-    "San Marino",
-    "Senegal",
-    "Somalia",
-    "Canada",
-    "Suriname",
-    "South Sudan",
-    "São Tomé And Príncipe",
-    "Democratic Republic Of The Congo",
-    "Central African Republic",
-    "El Salvador",
-    "Republic Of The Congo",
-    "Switzerland",
-    "Sint Maarten",
-    "Syria",
-    "Ivory Coast",
-    "Eswatini",
-    "Chile",
-    "Cameroon",
-    "People's Republic Of China",
-    "Colombia",
-    "Costa Rica",
-    "Chad",
-    "Cuba",
-    "Togo",
-    "Cape Verde",
-    "Thailand",
-    "Curaçao",
-    "Turkish Republic Of Northern Cyprus",
-    "Tajikistan",
-    "Czech Republic",
-    "East Timor",
-    "Turkmenistan",
-    "Tunisia",
-    "Tonga",
-    "Turkey",
-    "Trinidad And Tobago",
-    "Germany",
-    "Tuvalu",
-    "Taiwan",
-    "Tanzania",
-    "Djibouti",
-    "Denmark",
-    "Dominica",
-    "Dominican Republic",
-    "Ukraine",
-    "Uganda",
-    "Algeria",
-    "United States Of America",
-    "Ecuador",
-    "Estonia",
-    "Egypt",
-    "Uruguay",
-    "Uzbekistan",
-    "Vatican City",
-    "Eritrea",
-    "Saint Vincent And The Grenadines",
-    "Spain",
-    "Ethiopia",
-    "Venezuela",
-    "Vietnam",
-    "Vanuatu",
-    "Finland",
-    "Fiji",
-    "Federated States Of Micronesia",
-    "France",
-    "Gabon",
-    "United Kingdom",
-    "Samoa",
-    "Grenada",
-    "Georgia",
-    "Guernsey",
-    "Ghana",
-    "Greenland",
-    "The Gambia",
-    "Guinea",
-    "Equatorial Guinea",
-    "Greece",
-    "Guatemala",
-    "Guinea-Bissau",
-    "Guyana",
-    "Kosovo",
-    "Hong Kong",
-    "Honduras",
-    "Croatia",
-    "Haiti",
-    "Yemen",
-    "Hungary",
-    "Indonesia",
-    "Ireland",
-    "Israel",
-    "Isle Of Man",
-    "India",
-    "South Africa",
-    "Iraq",
-    "Iran",
-    "Iceland",
-    "Italy",
-    "Zambia",
-    "Jersey",
-    "Zimbabwe",
-    "Jamaica",
-    "Jordan",
-    "Japan",
-    "Kenya",
-    "Kyrgyzstan",
-    "Cambodia",
-    "Kiribati",
-    "Comoros",
-    "Saint Kitts And Nevis",
-    "North Korea",
-    "South Korea",
-    "Kuwait",
-    "Kazakhstan",
-    "Laos",
-    "Lebanon",
-    "Saint Lucia",
-    "Liechtenstein",
-    "Sri Lanka",
-    "Liberia",
-    "Lesotho",
-    "Lithuania",
-    "Luxembourg",
-    "Latvia",
-    "Libya",
-    "Morocco",
-    "Monaco",
-    "Moldova",
-    "Montenegro",
-    "Madagascar",
-    "Marshall Islands",
-    "Republic Of Macedonia",
-    "Mali",
-    "Myanmar",
-    "Mongolia",
-    "Macau",
-    "Mauritania",
-    "Malta",
-    "Mauritius",
-    "Maldives",
-    "Malawi",
-    "Mexico",
-    "Malaysia",
-    "Mozambique",
-    "Namibia",
-    "Niger",
-    "Nigeria",
-    "Nicaragua",
-    "Netherlands",
-    "Norway",
-    "Nepal",
-    "Nauru",
-    "New Zealand",
-    "Oman",
-    "Panama",
-    "Peru",
-    "Papua New Guinea",
-    "Philippines",
-    "Pakistan",
-    "Poland",
-  ];
-
-  const [outputData, setOutputData] = useState(data);
+  const [outputData, setOutputData] = useState([]);
 
   console.log(outputData);
+
+  useEffect(
+    function () {
+      if (isOpen && value.length > 0) {
+        setOutputData(function () {
+          return remoteData.filter(function (citem) {
+            return citem.toLowerCase().includes(value.toLowerCase());
+          });
+        });
+      }
+    },
+    [isOpen, value, remoteData, setOutputData]
+  );
+
+  useEffect(function () {
+    setOutputData(remoteData);
+  }, []);
+
   return (
     <ModalContext.Provider
       value={{
         isOpen,
         setIsOpen,
-        data,
+
         value,
         setValue,
         outputData,
         setOutputData,
+        remoteData,
+        isLoading,
       }}
     >
       {children}
@@ -278,6 +89,7 @@ const StyledDiv = styled.div`
   top: 50%;
   left: 50%;
   border-radius: 4px;
+
   height: 250px;
   width: 15%;
   overflow: scroll;
@@ -296,9 +108,17 @@ const StyledLi = styled.li`
   border-bottom: 2px solid #18212f;
 `;
 export function OutPutContainer() {
-  const { isOpen, data, setValue, setIsOpen, outputData } = useModalContext();
+  const {
+    isOpen,
+    setValue,
+    setIsOpen,
+    outputData,
+    isLoading,
+    remoteData,
+    value,
+  } = useModalContext();
 
-  if (isOpen) {
+  if (isOpen && !isLoading) {
     return createPortal(
       <StyledDiv>
         <ImCancelCircle
@@ -313,19 +133,21 @@ export function OutPutContainer() {
           }}
         />
         <ul>
-          {outputData?.map(function (citem, i) {
-            return (
-              <StyledLi
-                key={i}
-                onClick={function () {
-                  setValue(citem);
-                  setIsOpen(false);
-                }}
-              >
-                {<strong style={{ fontWeight: "lighter" }}>{citem}</strong>}
-              </StyledLi>
-            );
-          })}
+          {(isOpen ? (value.length > 0 ? outputData : remoteData) : []).map(
+            function (citem, i) {
+              return (
+                <StyledLi
+                  key={i}
+                  onClick={function () {
+                    setValue(citem);
+                    setIsOpen(false);
+                  }}
+                >
+                  {<strong style={{ fontWeight: "lighter" }}>{citem}</strong>}
+                </StyledLi>
+              );
+            }
+          )}
         </ul>
       </StyledDiv>,
       document.body
