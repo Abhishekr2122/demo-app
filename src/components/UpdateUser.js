f13edc129c170f55125d9bd7b3f120854df5c549
Abhishekr2122 <abhishekrshinde21@gmail.com>
2023-12-17 18:49:21 +0530

    Creating the user update form

---
+++ src/components/UpdateUser.js
@@ -0,0 +1,77 @@
+import Form from "../ui/Form";
+import FormRow from "../ui/FormRow";
+import Input from "../ui/Input";
+
+export default function UpdateUser() {
+  return (
+    <Form>
+      <FormRow>
+        <label>
+          <strong style={{ color: "white" }}>First Name</strong>
+        </label>
+        <Input type="text" id="firstName" defaultValue={"Abhishke"} />
+      </FormRow>
+      <FormRow>
+        <label>
+          <strong style={{ color: "white" }}>Last Name</strong>
+        </label>
+        <Input type="text" id="lastName" defaultValue={"Shinde"} />
+      </FormRow>
+
+      <FormRow>
+        <label>
+          <strong style={{ color: "white" }}>Email Address</strong>
+        </label>
+        <Input
+          type="email"
+          id="email"
+          defaultValue={"abhishekrshinde21@gmail.com"}
+        />
+      </FormRow>
+      <FormRow>
+        <label>
+          <strong style={{ color: "white" }}>Mobile</strong>
+        </label>
+        <Input type="tel" id="phonenumber" defaultValue={7720960310} />
+      </FormRow>
+      <FormRow>
+        <label>
+          <strong style={{ color: "white" }}>Country</strong>
+        </label>
+        <Input type="text" id="country" defaultValue={"india"} />
+      </FormRow>
+      <FormRow>
+        <label>
+          <strong style={{ color: "white" }}>State</strong>
+        </label>
+        <Input type="text" id="state" defaultValue={"Maharashtra"} />
+      </FormRow>
+      <FormRow>
+        <label>
+          <strong style={{ color: "white" }}>Address1</strong>
+        </label>
+        <Input
+          type="text"
+          id="address1"
+          defaultValue={"Panchkrishna nivas vatsalya nagar society nanded"}
+        />
+      </FormRow>
+      <FormRow>
+        <label>
+          <strong style={{ color: "white" }}>Address2</strong>
+        </label>
+        <Input
+          type="text"
+          id="address2"
+          defaultValue={"Near balaji temple nanded"}
+        />
+      </FormRow>
+      <FormRow>
+        <label>
+          <strong style={{ color: "white" }}>Zipcode</strong>
+        </label>
+        <Input type="number" id="address2" defaultValue={431605} />
+      </FormRow>
+    </Form>
+  );
+}
---
+++ src/ui/UpdateModal.js
@@ -0,0 +1,52 @@
+import { createContext, useContext, useState } from "react";
+import UpdateUser from "../components/UpdateUser";
+import { createPortal } from "react-dom";
+import styled from "styled-components";
+
+const StyledUpdateContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  border-radius: 4px;
+
+  height: 250px;
+  width: 60%;
+  overflow: scroll;
+  scroll-behavior: smooth;
+  background-color: #111827;
+  &::-webkit-scrollbar {
+    display: none;
+    scroll-behavior: smooth;
+  }
+`;
+
+const UserUpdateContext = createContext();
+
+export default function UpdateModal({ children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <UserUpdateContext.Provider value={{ isOpen, setIsOpen }}>
+      {children}
+    </UserUpdateContext.Provider>
+  );
+}
+
+export function useUpdateUser() {
+  const data = useContext(UserUpdateContext);
+  return data;
+}
+
+export function UpdateContainer() {
+  const { isOpen } = useUpdateUser();
+  if (isOpen) {
+    return createPortal(
+      <StyledUpdateContainer>
+        <UpdateUser />
+      </StyledUpdateContainer>,
+      document.body
+    );
+  } else {
+    return null;
+  }
+}
